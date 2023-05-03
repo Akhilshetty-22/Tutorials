f5ac670203abe6fcfbcaec03ff4c1a1974436d9f
Akhil-Shetty <settiakhil9@gmail.com>
2023-05-03 11:14:43 -0500

    First commit

---
+++ src/main/java/com/crudOperations/intellijdemopage/IntellijDemopageApplication.java
@@ -0,0 +1,13 @@
+package com.crudOperations.intellijdemopage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellijDemopageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellijDemopageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crudOperations/intellijdemopage/IntellijDemopageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudOperations.intellijdemopage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellijDemopageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
